360d73dad924e07789718e168794cf3fdcc17e90
Ronit Shinde <shinderonit4@gmail.com>
2026-03-11 21:46:21 +0530

    Converted deals api to CBV

--- crm/crmapp/views/deals_views.py
+++ crm/crmapp/views/deals_views.py
@@ -8,151 +8,300 @@ from ..models.user import Userprofile
 from ..models.deal import Deal
 from ..models.contact import Contact
 from ..decorators import login_is_required, role_required
+from django.utils.decorators import method_decorator
+from django.views import View
 
 """
     SWITCHING TOO DRF AND CLASS-BASED VIEWS
 """
 
-@login_is_required
-@csrf_exempt
-def create_deal(request):
-    if request.method != "POST":
-        return JsonResponse({"error": "POST request required"}, status=405)
 
-    if not request.user.is_authenticated:
-        return JsonResponse({"error": "Authentication required"}, status=401)
-
-    data = json.loads(request.body)
-    title = data.get("title")
-    value = data.get("value")
-    stage = data.get("stage")
-    expected_closed_date = data.get("closedate")
-    note = data.get("note")
-    profile = request.user.userprofile
-    company = profile.company
-    owner = request.user
-
-    with transaction.atomic():
-        Deal.objects.create(
-            owner=owner,
-            company=company,
-            title=title,
-            value=value,
-            stage=stage,
-            expected_closed_date=expected_closed_date,
-            note=note,
-        )
-
-    return JsonResponse({"message": "Deal created successfully"}, status=201)
+def login_and_csrf_exempt(view):
+    return method_decorator([csrf_exempt, login_is_required])(view)
 
 
-@login_is_required
-@csrf_exempt
-def get_all_deals(request):
-
-    if request.method != "GET":
-        return JsonResponse({"error": "GET request required"}, status=400)
-
-    deals = Deal.objects.filter(owner=request.user).select_related("company", "contact")
-
-    deal_data = []
-
-    for deal in deals:
-        deal_data.append(
-            {
-                "id": deal.id,
-                "company_id": deal.company.id,
-                "company_name": deal.company.title,
-                # "contact_id": deal.contact.id,
-                # "contact_name": deal.contact.name,  # change if different field ok beta samja kyaaaaaa
-                "value": deal.value,
-                "stage": deal.stage,
-                "expected_closed_date": str(deal.expected_closed_date),
-                "note": deal.note,
-                "created_at": str(deal.created_at),
-            }
-        )
-
-    return JsonResponse({"deals": deal_data}, status=200)
-
-
-@login_is_required
-@csrf_exempt
-def update_deal(request, deal_id):
-    if request.method != "POST":
-        return JsonResponse({"error": "POST request required"})
-
-    try:
-        company = request.user.userprofile.company
-        deal = Deal.objects.get(id=deal_id)
-        if not deal:
-            return JsonResponse({"error": "Deal does not exists"}, status=404)
-
-        if deal.company != company:
-            return JsonResponse(
-                {"error": "You cannot modify other company's data"}, status=401
-            )
+@login_and_csrf_exempt
+class DealCreateView(View):
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return JsonResponse({"error": "Authentication required"}, status=401)
 
         data = json.loads(request.body)
-        fields = ["title", "value", "stage", "expected_closed_date", "note"]
+        title = data.get("title")
+        value = data.get("value")
+        stage = data.get("stage")
+        expected_closed_date = data.get("closedate")
+        note = data.get("note")
+        profile = request.user.userprofile
+        company = profile.company
+        owner = request.user
 
-        for field in fields:
-            if field in data:
-                setattr(deal, field, data[field])
-
-        return JsonResponse(
-            {
-                "message": "Updated deal successfully",
-                "title": deal.title,
-                "value": deal.value,
-                "stage": deal.value,
-                "expected_close_date": deal.expected_closed_date,
-                "note": deal.note,
-            }
-        )
-
-    except Exception as e:
-        return JsonResponse({"error": str(e)}, status=400)
-
-
-@login_is_required
-@csrf_exempt
-def get_deal_by_id(request, deal_id):
-    if request.method != "GET":
-        return JsonResponse({"error": "GET request required"}, status=405)
-
-    try:
-        deal = Deal.objects.get(id=deal_id)
-
-        if not deal:
-            return JsonResponse({"error": "Deal does not exists"}, status=404)
-
-        company = request.user.userprofile.company
-        if deal.company != company:
-            return JsonResponse(
-                {"error": "Cannot modify other company's data"}, status=400
+        with transaction.atomic():
+            Deal.objects.create(
+                owner=owner,
+                company=company,
+                title=title,
+                value=value,
+                stage=stage,
+                expected_closed_date=expected_closed_date,
+                note=note,
             )
 
-        return JsonResponse(
-            {
-                "message": "Fetched deal successfully",
-                "title": deal.title,
-                "value": deal.value,
-                "stage": deal.stage,
-                "expected_closed_date": deal.expected_closed_date,
-                "note": deal.note,
-                "created_at": deal.created_at
-            }
-        )
-    except Exception as e:
-        return JsonResponse({"error": str(e)}, status=400)
+        return JsonResponse({"message": "Deal created successfully"}, status=201)
 
 
-@csrf_exempt
-def delete_deal(request):
-    pass
+@login_and_csrf_exempt
+class DealListView(View):
+    def get(self, request):
+        deals = Deal.objects.filter(owner=request.user).select_related("company", "contact")
+        deal_data = []
+
+        for deal in deals:
+            deal_data.append(
+                {
+                    "id": deal.id,
+                    "company_id": deal.company.id,
+                    "company_name": deal.company.title,
+                    "value": deal.value,
+                    "stage": deal.stage,
+                    "expected_closed_date": str(deal.expected_closed_date),
+                    "note": deal.note,
+                    "created_at": str(deal.created_at),
+                }
+            )
+
+        return JsonResponse({"deals": deal_data}, status=200)
 
 
-@csrf_exempt
-def reactivate_deal(request):
-    pass
+@login_and_csrf_exempt
+class DealDetailView(View):
+    def get(self, request, deal_id):
+        try:
+            deal = Deal.objects.get(id=deal_id)
+            company = request.user.userprofile.company
+
+            if deal.company != company:
+                return JsonResponse({"error": "Cannot access other company's data"}, status=400)
+
+            return JsonResponse(
+                {
+                    "message": "Fetched deal successfully",
+                    "title": deal.title,
+                    "value": deal.value,
+                    "stage": deal.stage,
+                    "expected_closed_date": deal.expected_closed_date,
+                    "note": deal.note,
+                    "created_at": deal.created_at
+                }
+            )
+
+        except Deal.DoesNotExist:
+            return JsonResponse({"error": "Deal does not exist"}, status=404)
+
+    def post(self, request, deal_id):
+        try:
+            deal = Deal.objects.get(id=deal_id)
+            company = request.user.userprofile.company
+
+            if deal.company != company:
+                return JsonResponse({"error": "Cannot modify other company's data"}, status=401)
+
+            data = json.loads(request.body)
+            fields = ["title", "value", "stage", "expected_closed_date", "note"]
+
+            for field in fields:
+                if field in data:
+                    setattr(deal, field, data[field])
+            deal.save()
+
+            return JsonResponse(
+                {
+                    "message": "Updated deal successfully",
+                    "title": deal.title,
+                    "value": deal.value,
+                    "stage": deal.stage,
+                    "expected_closed_date": deal.expected_closed_date,
+                    "note": deal.note,
+                }
+            )
+
+        except Deal.DoesNotExist:
+            return JsonResponse({"error": "Deal does not exist"}, status=404)
+
+
+@login_and_csrf_exempt
+class DealDeleteView(View):
+    def post(self, request, deal_id):
+        try:
+            deal = Deal.objects.get(id=deal_id)
+            company = request.user.userprofile.company
+
+            if deal.company != company:
+                return JsonResponse({"error": "Cannot delete other company's data"}, status=401)
+
+            deal.delete()
+            return JsonResponse({"message": "Deal deleted successfully"}, status=200)
+
+        except Deal.DoesNotExist:
+            return JsonResponse({"error": "Deal does not exist"}, status=404)
+
+
+@login_and_csrf_exempt
+class DealReactivateView(View):
+    def post(self, request, deal_id):
+        try:
+            deal = Deal.objects.get(id=deal_id)
+            company = request.user.userprofile.company
+
+            if deal.company != company:
+                return JsonResponse({"error": "Cannot reactivate other company's data"}, status=401)
+
+            deal.is_active = True
+            deal.save()
+            return JsonResponse({"message": "Deal reactivated successfully"}, status=200)
+
+        except Deal.DoesNotExist:
+            return JsonResponse({"error": "Deal does not exist"}, status=404)
+# @login_is_required
+# @csrf_exempt
+# def create_deal(request):
+#     if request.method != "POST":
+#         return JsonResponse({"error": "POST request required"}, status=405)
+
+#     if not request.user.is_authenticated:
+#         return JsonResponse({"error": "Authentication required"}, status=401)
+
+#     data = json.loads(request.body)
+#     title = data.get("title")
+#     value = data.get("value")
+#     stage = data.get("stage")
+#     expected_closed_date = data.get("closedate")
+#     note = data.get("note")
+#     profile = request.user.userprofile
+#     company = profile.company
+#     owner = request.user
+
+#     with transaction.atomic():
+#         Deal.objects.create(
+#             owner=owner,
+#             company=company,
+#             title=title,
+#             value=value,
+#             stage=stage,
+#             expected_closed_date=expected_closed_date,
+#             note=note,
+#         )
+
+#     return JsonResponse({"message": "Deal created successfully"}, status=201)
+
+
+# @login_is_required
+# @csrf_exempt
+# def get_all_deals(request):
+
+#     if request.method != "GET":
+#         return JsonResponse({"error": "GET request required"}, status=400)
+
+#     deals = Deal.objects.filter(owner=request.user).select_related("company", "contact")
+
+#     deal_data = []
+
+#     for deal in deals:
+#         deal_data.append(
+#             {
+#                 "id": deal.id,
+#                 "company_id": deal.company.id,
+#                 "company_name": deal.company.title,
+#                 # "contact_id": deal.contact.id,
+#                 # "contact_name": deal.contact.name,  # change if different field ok beta samja kyaaaaaa
+#                 "value": deal.value,
+#                 "stage": deal.stage,
+#                 "expected_closed_date": str(deal.expected_closed_date),
+#                 "note": deal.note,
+#                 "created_at": str(deal.created_at),
+#             }
+#         )
+
+#     return JsonResponse({"deals": deal_data}, status=200)
+
+
+# @login_is_required
+# @csrf_exempt
+# def update_deal(request, deal_id):
+#     if request.method != "POST":
+#         return JsonResponse({"error": "POST request required"})
+
+#     try:
+#         company = request.user.userprofile.company
+#         deal = Deal.objects.get(id=deal_id)
+#         if not deal:
+#             return JsonResponse({"error": "Deal does not exists"}, status=404)
+
+#         if deal.company != company:
+#             return JsonResponse(
+#                 {"error": "You cannot modify other company's data"}, status=401
+#             )
+
+#         data = json.loads(request.body)
+#         fields = ["title", "value", "stage", "expected_closed_date", "note"]
+
+#         for field in fields:
+#             if field in data:
+#                 setattr(deal, field, data[field])
+
+#         return JsonResponse(
+#             {
+#                 "message": "Updated deal successfully",
+#                 "title": deal.title,
+#                 "value": deal.value,
+#                 "stage": deal.value,
+#                 "expected_close_date": deal.expected_closed_date,
+#                 "note": deal.note,
+#             }
+#         )
+
+#     except Exception as e:
+#         return JsonResponse({"error": str(e)}, status=400)
+
+
+# @login_is_required
+# @csrf_exempt
+# def get_deal_by_id(request, deal_id):
+#     if request.method != "GET":
+#         return JsonResponse({"error": "GET request required"}, status=405)
+
+#     try:
+#         deal = Deal.objects.get(id=deal_id)
+
+#         if not deal:
+#             return JsonResponse({"error": "Deal does not exists"}, status=404)
+
+#         company = request.user.userprofile.company
+#         if deal.company != company:
+#             return JsonResponse(
+#                 {"error": "Cannot modify other company's data"}, status=400
+#             )
+
+#         return JsonResponse(
+#             {
+#                 "message": "Fetched deal successfully",
+#                 "title": deal.title,
+#                 "value": deal.value,
+#                 "stage": deal.stage,
+#                 "expected_closed_date": deal.expected_closed_date,
+#                 "note": deal.note,
+#                 "created_at": deal.created_at
+#             }
+#         )
+#     except Exception as e:
+#         return JsonResponse({"error": str(e)}, status=400)
+
+
+# @csrf_exempt
+# def delete_deal(request):
+#     pass
+
+
+# @csrf_exempt
+# def reactivate_deal(request):
+#     pass
--- crm/crmapp/views/task_view.py
+++ crm/crmapp/views/task_view.py
@@ -1,22 +1,5 @@
-<<<<<<< HEAD
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
-from ..models.contact import Contact
-from..models.task import Task
-from..models.deal import Deal
-from ..models.lead import Lead
-from ..models.user import Userprofile
-from django.contrib.contenttypes.models import ContentType
-import json
-from django.contrib.auth.models import User
-from django.utils.dateparse import parse_date
-=======
 from ..models.task import Task
 from django.contrib.contenttypes.models import ContentType
->>>>>>> f151a956b9d45ab36ba55c2f8f4e44255deb6d96
 
 """
     SWITCHING TOO DRF AND CLASS-BASED VIEWS
@@ -26,33 +9,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
-<<<<<<< HEAD
-@csrf_exempt
-def get_all_tasks(request):
-    if request.method != 'POST':
-      return JsonResponse({"error": "POST request required"}, status=405)
-    try :
-      user_company=request.user.Userprofile.company
-      #is company ke sare task ayege
-      tasks=Task.objects.filter(company=user_company)
-      task_list=[]
-      
-      for task in tasks:#basic operation
-        task_list.append({
-          "id":task.id,
-          "description":task.description,
-          "status":task.status,
-          "duedate":task.duedate,
-          "assignedTo":task.assignedTo.username if task.assignedTo else None
-        })
-      return JsonResponse({"tasks":task_list},status=200)
-    except Exception as e:
-      return JsonResponse({"error":str(e)},status=500)
-    #aasaaan haiiiiii
-      
-
-
-=======
 from ..serializers.task_serializers import TaskSerializer
 from ..utils.disable_csrf import CsrfExemptSessionAuthentication
 
@@ -176,7 +132,6 @@ class TaskViewSet(ModelViewSet):
 
 """
 @login_is_required
->>>>>>> f151a956b9d45ab36ba55c2f8f4e44255deb6d96
 @csrf_exempt
 def create_task(request):
     if request.method != 'POST':
@@ -243,109 +198,4 @@ def create_task(request):
 
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
-<<<<<<< HEAD
-
-@csrf_exempt
-def update_task(request,task_id):
-    if request.method!='POST':
-      return JsonResponse({"error":"post request required"},status=405)
-    
-    try:
-      data=json.loads(request.body)
-      #lets lake all task 1
-      try:
-        task=Task.objects.get(id=task_id)
-      except Task.DoesNotExist:
-        data=json.loads(request.data)
-      user_company=request.user.userprofile.company
-      if task.company!=user_company:
-        return JsonResponse({"error": "Unauthorized access"}, status=403)
-      
-      #this fields can be updated samjha kya lala?
-      description=data.get("description")
-      duedate=data.get("duedate")
-      status=data.get("status")
-      assigned_to_id=data.get("assigned_to")
-      
-      if description:
-        task.description=description
-      if duedate:
-        parsed_date=parse_date(duedate)
-        if not parsed_date:
-          return JsonResponse({"error": "Invalid date format (YYYY-MM-DD required)"}, status=400) 
-        task.duedate=duedate
-      if status:
-        task.status=status 
-      
-    allowed_fields = ["description", "duedate", "status", "assigned_to_id"]
-
-    for field in allowed_fields:
-        if field in data:
-            setattr(task, field, data[field])
-      if assigned_to_id is not None:
-        if assigned_to_id=="":
-          task.assignedTo=None #this will work when user will erase the old assigned person and enters nothing 
-        else:
-          try:
-            assigned_user=User.objects.get(id=assigned_to_id)
-            task.assignedTo=assigned_user
-          except User.DoesNotExist:
-            return JsonResponse({"error": "Assigned user not found"}, status=404)
-      task.save()
-      return JsonResponse({"message": "Task updated successfully"}, status=200)
-    
-    except Exception as e:
-        return JsonResponse({"error": str(e)}, status=500)#here e the any specific error
-        
-  
-
-@csrf_exempt
-def delete_task(request,task_id):
-    if request.method!='POST':
-      return JsonResponse({"error":"post request required"},status=405)
-    try:
-      try:
-        task=Task.objects.get(id=task_id)
-      except Task.DoesNotExist:
-        return JsonResponse({"error":"Task not found"},status=404)
-      user_company=request.user.Userprofile.company 
-      if task.company!=user_company:
-        return JsonResponse({"error":"Unauthorized access"},status=403)
-      
-      task.delete()
-      return JsonResponse({"message":"task deleted successfully"},status=200)
-    
-    except Exception as e :
-      return JsonResponse({"error":str(e)},status=500)
-
-@csrf_exempt
-def get_task_by_id(request,task_id):#ye pura khudse kiya hai thoda goThrough karna padega
-    if request.method!='POST':
-      return JsonResponse({"error":"post request required"},status=405)
-    try :
-      data=json.loads(request.body)
-      try:
-        task=Task.objects.get(id=task_id)
-      except Task.DoesNotExist:
-        return JsonResponse({"error":"Task not found"},status=404)
-      user_company=request.user.Userprofile.company
-      if task.company!=user_company:
-        return JsonResponse({"error":"Unauthorized access"},status=403)
-      
-      return JsonResponse({
-        "id" :task.id,
-        "description":task.description,
-        "status":task.status,
-        "duedate":task.duedate,
-        "assignedTo":task.assignedTo.username if task.assignedTo else None
-      },status=200)
-    except Exception as e :
-      return JsonResponse({"error":str(e)},status=500)
-@csrf_exempt
-def reactivate_task(request):
-  return JsonResponse({
-    "error": "This function is still in hold and in progress"
-  })
-=======
 """
->>>>>>> f151a956b9d45ab36ba55c2f8f4e44255deb6d96
